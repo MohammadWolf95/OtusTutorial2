f308536ae25a3a9725f982bb963210313730f697
MohammadWolf95 <abdulavadudovmagomed@mail.ru>
2018-03-22 22:43:08 +0300

    commit 42

--- filter.cpp
+++ filter.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<vector<string>> filter_any(const vector<vector<string>>&ip_pool, int find_byte){
     vector<vector<string>>vec;
     for(const auto&i:ip_pool){
-        auto it = find(i.begin(), i.end(), to_string(find_byte));
+        auto it = find(i.begin(), i.end(), std::to_string(find_byte));
         if(it!=i.end())
             vec.emplace_back(i);
     }
